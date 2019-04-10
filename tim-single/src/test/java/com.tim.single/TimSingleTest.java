4b44700babe37876df21ce95257f7a0130c7cb03
baibing <baibing@rongcloud.cn>
2019-04-10 19:19:43 +0800

    use spring boot test structure.

--- tim-single/src/test/java/com.tim.single/TimSingleTest.java
+++ tim-single/src/test/java/com.tim.single/TimSingleTest.java
@@ -15,7 +15,13 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static junit.framework.TestCase.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
 public class TimSingleTest {
 

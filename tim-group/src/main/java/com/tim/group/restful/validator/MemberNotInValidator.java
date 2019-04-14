86e02134fe8085f6354ad5978ae3cb9ccbb7fd98
baibing <baibing@rongcloud.cn>
2019-04-14 21:35:07 +0800

    fix code standard issue.

--- tim-group/src/main/java/com/tim/group/restful/validator/MemberNotInValidator.java
+++ tim-group/src/main/java/com/tim/group/restful/validator/MemberNotInValidator.java
@@ -2,11 +2,8 @@ package com.tim.group.restful.validator;
 
 import com.tim.common.result.ResultCode;
 import com.tim.group.restful.bean.model.GroupMemberModel;
-import com.tim.group.restful.bean.model.GroupModel;
 import com.tim.group.restful.mapper.GroupMemberMapper;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import tk.mybatis.mapper.entity.Example;
@@ -22,8 +19,6 @@ public class MemberNotInValidator implements Validator {
     @Autowired
     private GroupMemberMapper memberMapper;
 
-    private List<GroupMemberModel> models;
-
     @Override
     public boolean isValid(String groupId, List<String> members) {
         //members 中有一个成员 不在群组中，就算失败。 members需要是一个净删除列表
@@ -32,7 +27,7 @@ public class MemberNotInValidator implements Validator {
             .andNotEqualTo("status", 2)
             .andEqualTo("groupId", groupId)
             .andIn("memberUid", members);
-        models = memberMapper.selectByExample(example);
+        List<GroupMemberModel> models = memberMapper.selectByExample(example);
         return models.size() == members.size();
     }
 

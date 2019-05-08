ca351305f9e3ee8d2a576ad3f1e07001bd625695
baibing <baibing@rongcloud.cn>
2019-05-08 11:32:47 +0800

    support CORS.

--- raven-route/src/main/java/com/raven/route/config/web/WebMvcConfig.java
+++ raven-route/src/main/java/com/raven/route/config/web/WebMvcConfig.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -17,4 +18,15 @@ public class WebMvcConfig extends WebMvcConfigurationSupport implements Environm
         this.environment = environment;
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+            .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin",
+                "Access-Control-Request-Method", "Access-Control-Request-Headers")
+            .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+            .allowCredentials(true)
+            .maxAge(1800);
+    }
 }

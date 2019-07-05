95a707ac15ec71c3f751cc6e16533d8dd8e3557a
IamNotShady <zhou.xiaoxiao@outlook.com>
2019-07-05 23:34:23 +0800

    fix

--- raven-single/src/main/java/com/raven/single/tcp/manager/SingleMessageProcessor.java
+++ raven-single/src/main/java/com/raven/single/tcp/manager/SingleMessageProcessor.java
@@ -15,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
-@Component
 public class SingleMessageProcessor implements Runnable {
 
     private ServerChannelManager internalServerChannelManager;

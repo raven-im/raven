00c3cf932e24337411b861a01ba41dfdb8c79521
baibing <baibing@rongcloud.cn>
2019-07-29 15:37:03 +0800

    add nacos client for config server.

--- raven-file/src/main/java/com/raven/file/RavenFileApplication.java
+++ raven-file/src/main/java/com/raven/file/RavenFileApplication.java
@@ -1,11 +1,13 @@
 package com.raven.file;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@NacosPropertySource(dataId = "raven-file.yaml")
 public class RavenFileApplication {
 
     public static void main(String[] args) {

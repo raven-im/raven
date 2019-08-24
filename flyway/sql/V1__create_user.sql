054c738d223ae23d3e33fc8777c61499597fdd31
IamNotShady <zhou.xiaoxiao@outlook.com>
2019-08-24 14:36:55 +0800

    mysql8 has deprecated Integer display width

--- flyway/sql/V1__create_user.sql
+++ flyway/sql/V1__create_user.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `t_user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ID',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'ID',
   `uid` varchar(36)  NOT NULL,
   `username` varchar(64) NOT NULL COMMENT '登录名',
   `password` varchar(64) NOT NULL COMMENT '登录密码',
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `t_user` (
   `email` varchar(64) NOT NULL DEFAULT "" COMMENT '邮箱',
   `name` varchar(16) NOT NULL DEFAULT "" COMMENT '名字',
   `portrait_url` varchar(1024) NOT NULL DEFAULT "" COMMENT '头像url',
-  `status` tinyint(1) NOT NULL DEFAULT '0' COMMENT '用户状态: 0: 正常 1: 禁用',
+  `status` tinyint NOT NULL DEFAULT '0' COMMENT '用户状态: 0: 正常 1: 禁用',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_username` (`username`),
   UNIQUE KEY `uk_uid` (`uid`)

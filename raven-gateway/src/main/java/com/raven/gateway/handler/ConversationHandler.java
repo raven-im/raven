53f937c60910850cb90b416c68fdd943752700a8
baibing <baibing@rongcloud.cn>
2019-09-16 19:27:56 +0800

    fix  Group create ,only notification sent.  no message ,no content. so no last message

--- raven-gateway/src/main/java/com/raven/gateway/handler/ConversationHandler.java
+++ raven-gateway/src/main/java/com/raven/gateway/handler/ConversationHandler.java
@@ -59,7 +59,7 @@ public class ConversationHandler extends SimpleChannelInboundHandler<RavenMessag
     }
 
     private void processConverReqMsg(ChannelHandlerContext ctx, ConverReq conversationReq) {
-        log.debug("receive conver request message:{}", JsonHelper.toJsonString(conversationReq));
+        log.info("receive conver request message:{}", JsonHelper.toJsonString(conversationReq));
         if (conversationReq.getType() == OperationType.DETAIL) {
             UserConversation userConversation = converManager
                 .getConverListInfo(uidChannelManager.getIdByChannel(ctx.channel()),
@@ -110,13 +110,15 @@ public class ConversationHandler extends SimpleChannelInboundHandler<RavenMessag
             builder.setGroupId(userConversation.getGroupId());
         }
         MsgContent msgContent = userConversation.getLastContent();
-        MessageContent content = MessageContent.newBuilder().setId(msgContent.getId())
-            .setUid(msgContent.getUid())
-            .setType(MessageType.valueOf(msgContent.getType()))
-            .setContent(msgContent.getContent())
-            .setTime(msgContent.getTime())
-            .build();
-        builder.setLastContent(content);
+        if (msgContent != null) {
+            MessageContent content = MessageContent.newBuilder().setId(msgContent.getId())
+                .setUid(msgContent.getUid())
+                .setType(MessageType.valueOf(msgContent.getType()))
+                .setContent(msgContent.getContent())
+                .setTime(msgContent.getTime())
+                .build();
+            builder.setLastContent(content);
+        }
         builder.setReadMsgId(userConversation.getReadMsgId());
         ConverInfo info = builder.build();
         return info;
